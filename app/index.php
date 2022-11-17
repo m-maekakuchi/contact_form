d76368d956489c4d8862e7209fb18be7e4c0cda3
m-maekakuchi <m.marikoromo@gmail.com>
2022-11-17 17:18:22 +0900

    二重送信防止のためのトークンを追加

--- app/index.php
+++ app/index.php
@@ -6,10 +6,8 @@
   require_once('utilities/ContactModel.php');
   require_once('utilities/functions.php');
 
-  
   try {
     $btn = "";
-    $inputData;
 
     if (array_key_exists('btn', $_POST)) {
       $btn = $_POST['btn'];
@@ -17,13 +15,6 @@
       $btn = $_GET['btn'];
     }
 
-    //入力データがセッションに登録されていれば変数$inputdataに代入
-    // if (isset($_SESSION['inputData'])) {
-    //   $inputData = $_SESSION['inputData'];
-    // } else {
-    //   $inputData = [];
-    // }
-    
     //正しいパラメータ名を受け取れなかった場合はお問い合わせフォームにリダイレクト
     if (!empty($btn)) {
       if ($btn === "toConfirm") {
@@ -59,12 +50,12 @@
         $val = new Validation();
         $errorMsg = $val->validateForms($name, $kana, $tel, $gender, $email, $confirmEmail, $content);
 
-        $inputData['name'] = (!isset($errorMsg['name'])) ? $name : "";
-        $inputData['kana'] = (!isset($errorMsg['kana'])) ? $kana : "";
-        $inputData['tel']  = (!isset($errorMsg['tel'])) ? $tel : "";
-        $inputData['email']  = (!isset($errorMsg['email'])) ? $email : "";
+        $inputData['name']          = (!isset($errorMsg['name'])) ? $name : "";
+        $inputData['kana']          = (!isset($errorMsg['kana'])) ? $kana : "";
+        $inputData['tel']           = (!isset($errorMsg['tel'])) ? $tel : "";
+        $inputData['email']         = (!isset($errorMsg['email'])) ? $email : "";
         $inputData['confirmEmail']  = (!isset($errorMsg['confirmEmail'])) ? $confirmEmail : "";
-        $inputData['content']  = (!isset($errorMsg['content'])) ? $content : "";
+        $inputData['content']       = (!isset($errorMsg['content'])) ? $content : "";
         if (!isset($errorMsg['gender'])) $inputData['gender'] = $gender;
 
         $_SESSION['inputData'] = $inputData;
@@ -80,48 +71,38 @@
           //改行コードを変換した文字列を変数に代入
           $_SESSION['confirmContent'] = str_replace("\n", "<br>", $inputData['content']);
 
-          // require_once('confirm.php');
           header('Location: confirm.php');
           exit();
         } else {
-          // require_once('contact.php');
           header('Location: contact.php');
           exit();
         }
       } else if ($btn === "toComplete") {
         //POSTされたトークンを取得
-        $postToken = isset($_POST['toen']) ? $_POST["token"] : "";
-        echo $_POST['token'];
+        $postToken = isset($_POST['token']) ? $_POST["token"] : "";
+
         //セッション変数のトークンを取得
-        $sessionToken = isset($_SESSION['toen']) ? $_SESSION["token"] : "";
-        echo $_SESSION['token'];
+        $sessionToken = isset($_SESSION['token']) ? $_SESSION['token'] : "";
 
         //セッション変数のトークンを削除
-        unset($_SESSION["token"]);
+        unset($_SESSION['token']);
 
-
+        $inputData = $_SESSION['inputData'];
         if($postToken !== "" && $postToken === $sessionToken) {
-          echo "DB登録GO!";
-        } else {
-          echo "不正な登録処理です";
+          $contactModel = new ContactModel();
+          $insertRow = $contactModel->insertInputData($inputData);
         }
 
-
-        $contactModel = new ContactModel();
-        // $insertRow = $contactModel->insertInputData($inputData);
-        // require_once('complete.php');
         header('Location: complete.php');
         exit();
       } else if ($btn === "back") {
         $_SESSION = array();
         session_destroy();
-        // require_once('contact.php');
+
         header('Location: contact.php');
         exit();
       }
     } else {
-      echo 103;
-      // require_once('contact.php');
       header('Location: contact.php');
       exit();
     }
