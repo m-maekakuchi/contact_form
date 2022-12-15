7ba546607cf0438c4441a7e5147a15a117b17cf4
m-maekakuchi <m.marikoromo@gmail.com>
2022-12-15 20:58:54 +0900

    レスポンシブデザイン追加

--- app/contact.php
+++ app/contact.php
@@ -22,123 +22,120 @@
 <body>
   <div class="container">
     <form action="index.php" method="post">
-      <div class="form-items item-pair">
-        <div class="item">
-          <label class="label-item">名前&emsp;<span class="label-required">必須</span></label>
-          <div class="item-column">
-            <input
-              type="text"
-              name="name"
-              id="name"
-              size="30"
-              value="<?= isset($inputData['name']) ? $inputData['name'] : "" ?>"
-            />
-            <?= isset($errorMsg['name']) ? "<span class ='error'>{$errorMsg['name']}</span>" : "" ?>
-          </div>
-        </div>
-        <div class="item">
-          <label class="label-item">フリガナ&emsp;<span class="label-required">必須</span></label>
-          <div class="item-column">
-            <input
-              type="text"
-              name="kana"
-              id="kana"
-              size="30"
-              value="<?= isset($inputData['kana']) ? $inputData['kana'] : "" ?>"
-            />
-            <?= isset($errorMsg['kana']) ? "<span class ='error'>{$errorMsg['kana']}</span>" : "" ?>
-          </div>
-        </div>
-      </div>
-      <div class="form-items item">
-        <label class="label-item">電話番号&emsp;<span class="label-required">必須</span></label>
+      <div class="form-items">
         <div class="item-column">
-          <input 
+          <div class="item-name">
+            名前&emsp;<span class="label-required">必須</span>
+          </div>
+          <input
             type="text"
-            name="tel"
-            id="tel"
-            size="30"
-            placeholder="000-0000-0000"
-            value="<?= isset($inputData['tel']) ? $inputData['tel'] : "" ?>"
+            name="name"
+            id="name"
+            size="38"
+            value="<?= isset($inputData['name']) ? $inputData['name'] : "" ?>"
           />
-          <?= isset($errorMsg['tel']) ? "<span class ='error'>{$errorMsg['tel']}</span>" : "" ?>
+          <?= isset($errorMsg['name']) ? "<span class ='error'>{$errorMsg['name']}</span>" : "" ?>
         </div>
-      </div>
-      <div class="form-items item">
-        <label class="label-item">性別&emsp;<span class="label-required">必須</span></label>
         <div class="item-column">
-          <div>
-            <label>
-              <input
-                type="radio"
-                name="gender"
-                value="男"
-                id="man"
-                <?= !isset($inputData['gender']) ? "checked" : "" ?>
-                <?= isset($inputData['gender']) && $inputData['gender'] !== '女' ? "checked" : "" ?>
-              >
-              <span class="gender">男性</span>
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="gender"
-                value="女"
-                id="woman"
-                <?= isset($inputData['gender']) && $inputData['gender'] == '女' ? "checked" : "" ?>
-              >
-              女性
-            </label>
+          <div class="item-name">
+            フリガナ&emsp;<span class="label-required">必須</span>
           </div>
-          <?= isset($errorMsg['gender']) ? "<span class ='error'>{$errorMsg['gender']}</span>" : "" ?>
+          <input
+            type="text"
+            name="kana"
+            id="kana"
+            size="38"
+            value="<?= isset($inputData['kana']) ? $inputData['kana'] : "" ?>"
+          />
+          <?= isset($errorMsg['kana']) ? "<span class ='error'>{$errorMsg['kana']}</span>" : "" ?>
         </div>
       </div>
-      <div class="form-items item-pair">
-        <div class="item">
-          <label class="label-item">メールアドレス&emsp;<span class="label-required">必須</span></label>
-          <div class="item-column">
+      <div class="form-items item-column">
+        <div class="item-name">
+          電話番号&emsp;<span class="label-required">必須</span>
+        </div>
+        <input 
+          type="text"
+          name="tel"
+          id="tel"
+          size="38"
+          placeholder="000-0000-0000"
+          value="<?= isset($inputData['tel']) ? $inputData['tel'] : "" ?>"
+        />
+        <?= isset($errorMsg['tel']) ? "<span class ='error'>{$errorMsg['tel']}</span>" : "" ?>
+      </div>
+      <div class="form-items item-column">
+        <div class="item-name">
+          性別&emsp;<span class="label-required">必須</span>
+        </div>
+        <div>
+          <label>
             <input
-              type="text"
-              name="email"
-              id="email"
-              size="50"
-              placeholder="example@gmail.com"
-              value="<?= isset($inputData['email']) ? $inputData['email'] : "" ?>"
-            />
-            <?= isset($errorMsg['email']) ? "<span class ='error'>{$errorMsg['email']}</span>" : "" ?>
-          </div>
-        </div>
-        <div class="item">
-          <label class="label-item">メールアドレス確認用&emsp;<span class="label-required">必須</span></label>
-          <div class="item-column">
+              type="radio"
+              name="gender"
+              value="男"
+              id="man"
+              <?= !isset($inputData['gender']) ? "checked" : "" ?>
+              <?= isset($inputData['gender']) && $inputData['gender'] !== '女' ? "checked" : "" ?>
+            >
+            <span class="gender">男性</span>
+          </label>
+          <label>
             <input
-              type="text"
-              name="confirmEmail"
-              id="confirmEmail"
-              size="50"
-              placeholder="example@gmail.com"
-              value="<?= isset($inputData['confirmEmail']) ? $inputData['confirmEmail'] : "" ?>"
-            />
-            <?= isset($errorMsg['confirmEmail']) ? "<span class ='error'>{$errorMsg['confirmEmail']}</span>" : "" ?>
+              type="radio"
+              name="gender"
+              value="女"
+              id="woman"
+              <?= isset($inputData['gender']) && $inputData['gender'] == '女' ? "checked" : "" ?>
+            >
+            女性
+          </label>
+        </div>
+        <?= isset($errorMsg['gender']) ? "<span class ='error'>{$errorMsg['gender']}</span>" : "" ?>
+      </div>
+      <div class="form-items">
+        <div class="item-column">
+          <div class="item-name">
+            メールアドレス&emsp;<span class="label-required">必須</span>
           </div>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            size="38"
+            placeholder="example@gmail.com"
+            value="<?= isset($inputData['email']) ? $inputData['email'] : "" ?>"
+          />
+          <?= isset($errorMsg['email']) ? "<span class ='error'>{$errorMsg['email']}</span>" : "" ?>
+        </div>
+        <div class="item-column">
+          <div class="item-name">
+            メールアドレス確認用&emsp;<span class="label-required">必須</span>
+          </div>
+          <input
+            type="text"
+            name="confirmEmail"
+            id="confirmEmail"
+            size="38"
+            placeholder="example@gmail.com"
+            value="<?= isset($inputData['confirmEmail']) ? $inputData['confirmEmail'] : "" ?>"
+          />
+          <?= isset($errorMsg['confirmEmail']) ? "<span class ='error'>{$errorMsg['confirmEmail']}</span>" : "" ?>
         </div>
       </div>
-      <div class="form-items item">
-        <label class="label-item">趣味</label>
-        <div class="item-column">
-          <div id="hobby">
-            <?= isset($inputData['hobbys']) ? getHtmlHobby($inputData['hobbys']) : getHtmlHobby(array()); ?>
-          </div>
-          <?= isset($errorMsg['hobby']) ? "<span class ='error'>{$errorMsg['hobby']}</span>" : "" ?>
-        </select>
+      <div class="form-items item-column">
+        <div class="item-name">趣味</div>
+        <div id="hobby">
+          <?= isset($inputData['hobbys']) ? getHtmlHobby($inputData['hobbys']) : getHtmlHobby(array()); ?>
         </div>
+        <?= isset($errorMsg['hobby']) ? "<span class ='error'>{$errorMsg['hobby']}</span>" : "" ?>
       </div>
-      <div class="form-items item">
-        <label class="label-item">お問い合わせ&emsp;<span class="label-required">必須</span></label>
-        <div class="item-column">
-          <textarea name="content" id="content" cols="50" rows="10"><?= isset($inputData['content']) ? $inputData['content'] : "" ?></textarea>
-          <?= isset($errorMsg['content']) ? "<span class ='error'>{$errorMsg['content']}</span>" : "" ?>
+      <div class="form-items item-column">
+        <div class="item-name">
+          お問い合わせ&emsp;<span class="label-required">必須</span>
         </div>
+        <textarea name="content" id="content" cols="40" rows="10"><?= isset($inputData['content']) ? $inputData['content'] : "" ?></textarea>
+        <?= isset($errorMsg['content']) ? "<span class ='error'>{$errorMsg['content']}</span>" : "" ?>
       </div>
       <div class="btn-wrapper">
         <button type="submit" class="btn">入力確認</button>
